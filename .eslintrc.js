a2a320498b32a4fc712fc58470a8779ff7fd4dbd
Xiaoerheiheizi <463726190@qq.com>
2019-07-23 17:58:36 +0800

    task # 更改配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,10 +21,10 @@ module.exports = {
   ],
   // add your custom rules here
   rules        : {
+      "indent": [1, 4],
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger'           : process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-tabs'               : 'off',
   }
 }
